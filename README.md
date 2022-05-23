22a444d479b9aa28dcb95a4de838b00ee7e04549
Bodin <bodintiago@gmail.com>
2022-05-24 00:55:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# This is an online-payment component
+
+The component would accept multiple payment methods
+
+
+#### Would Support:
+- Stripe //In development
+- Paypal //In development
+- Wire transfer //In development
---
+++ main.py
@@ -0,0 +1,13 @@
+class PaymentSolution:
+    def __init__(self, solution, amount, sender, receiver):
+        self.solution = solution
+        self.amount = amount
+        self.sender = sender
+        self.receiver = receiver
+
+    def paypal_solution(self):
+        pass
+
+    def stripe_solution(self):
+        pass
+
