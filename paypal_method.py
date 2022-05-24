86c03ace846b11586dedca6cf7858f66341be1c2
Bodin <bodintiago@gmail.com>
2022-05-25 01:10:58 +0200

    refactor paypal payment into new file

---
+++ paypal_method.py
@@ -0,0 +1,42 @@
+import os
+
+import paypalrestsdk
+
+SELLER_PAYPAL_ID = os.environ.get("SELLER_PAYPAL_ID")
+SELLER_PAYPAL_KEY = os.environ.get("SELLER_PAYPAL_KEY")
+
+
+class PaypalSolution:
+    def __init__(self, data):
+        self.amount = data["amount"]
+        self.currency = data["currency"]
+        self.sender = data["sender"]
+        self.receiver = data["receiver"]
+
+    def issue_payment(self):
+        paypalrestsdk.configure({
+            "mode": "sandbox",  # sandbox or live
+            "client_id": SELLER_PAYPAL_ID,
+            "client_secret": SELLER_PAYPAL_KEY})
+
+        payment = paypalrestsdk.Payment({
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"},
+            "transactions": [{
+                "item_list": {
+                    "items": [{
+                        "name": "item",
+                        "sku": "item",
+                        "price": self.amount,
+                        "currency": self.currency,
+                        "quantity": 1}]},
+                "amount": {
+                    "total": self.amount,
+                    "currency": self.currency},
+                "description": "This is the payment transaction description."}]})
+
+        if payment.create():
+            print("Payment created successfully")
+        else:
+            print(payment.error)
