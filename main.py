54e8b92e0f9aa2e8b8a14eed7bd2961ed84fabd4
Bodin <bodintiago@gmail.com>
2022-05-24 15:00:23 +0200

    add charge method for stripe

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import os
+
 import stripe
 
 
+SELLER_STRIPE_KEY = os.environ.get("SELLER_STRIPE_KEY")
+
+class PaymentGateway:
+    def __init__(self, method, data):
+        if method == "paypal":
+            PaypalSolution(data)
+
+        elif method == "stripe":
+            StripeSolution(data)
+
+"""
 class PaymentSolution:
     def __init__(self, method, data):
         self.method = method
@@ -8,16 +21,17 @@ class PaymentSolution:
         self.choose_solution()
 
     def choose_solution(self):
-        if self.data["method"] == "paypal":
+        if self.method == "paypal":
             PaypalSolution(self.data)
 
-        elif self.data["method"] == "stripe":
+        elif self.method == "stripe":
             StripeSolution(self.data)
-
+"""
 
 class PaypalSolution:
     def __init__(self, data):
         self.amount = data["amount"]
+        self.currency = data["currency"]
         self.sender = data["sender"]
         self.receiver = data["receiver"]
 
@@ -25,5 +39,20 @@ class PaypalSolution:
 class StripeSolution:
     def __init__(self, data):
         self.amount = data["amount"]
+        self.currency = data["currency"]
         self.sender = data["sender"]
         self.receiver = data["receiver"]
+        self.api_key = data["api_key"]
+
+    def send_order(self):
+        stripe.api_key = self.api_key
+        try:
+            charge = stripe.Charge.create(
+                amount=self.amount,
+                currency=self.currency,
+                description="Customer buy an item",
+                source=SELLER_STRIPE_KEY,
+                idempotency_key=''
+            )
+        except:
+            raise ValueError("Missing or illegal value in data")
