c553b90fd26dfece1d75553949b518918098b53c
Bodin <bodintiago@gmail.com>
2022-05-25 16:55:43 +0200

    new file from refactoring main.py gateway class

---
+++ gateway.py
@@ -0,0 +1,11 @@
+from paypal_method import PaypalSolution
+from stripe_method import StripeSolution
+
+
+class PaymentGateway:
+    def __init__(self, method, data):
+        if method == "paypal":
+            PaypalSolution(data)
+
+        elif method == "stripe":
+            StripeSolution(data)
